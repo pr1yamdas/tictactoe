951c1ea8f6bf1472a1ce13606210a5d7675e1329
pr1yamdas <131381963+pr1yamdas@users.noreply.github.com>
2023-07-10 16:32:11 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/tictac/MainActivity.java
+++ app/src/main/java/com/example/tictac/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.tictac;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,29 +9,37 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean gameactive= true;
+    boolean gameactive = true;
     int activeplayer = 0;
-    int[] gamestate = { 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+    int[] gamestate = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winpositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
+    private Button resetButton;
 
-    int[][] winpositions = { {0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8},
-                                        {0,4,8}, {2,4,6}  };
-    private Button resetbutton;
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resetButton = findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gamereset(v);
+            }
+        });
+    }
 
     public void playertap(View view) {
-
         ImageView img = (ImageView) view;
         int tappedimage = Integer.parseInt(img.getTag().toString());
-        if(!gameactive){
+
+        if (!gameactive) {
             gamereset(view);
         }
 
-
-
-
         if (gamestate[tappedimage] == 2 && gameactive) {
             gamestate[tappedimage] = activeplayer;
+
             if (activeplayer == 0) {
                 img.setImageResource(R.drawable.x);
                 activeplayer = 1;
@@ -45,27 +52,23 @@ public class MainActivity extends AppCompatActivity {
                 status.setText("X's turn");
             }
 
-        }
-
-        for(int[] winposition: winpositions){
-            if(gamestate[winposition[0]] == gamestate[winposition[1]] && gamestate[winposition[1]] == gamestate[winposition[2]] && gamestate[winposition[0]] != 2){
-                String winnerstr;
-                gameactive = false;
-                if (gamestate[winposition[0]] == 0){
-                    winnerstr = "X is the winner";
+            for (int[] winposition : winpositions) {
+                if (gamestate[winposition[0]] == gamestate[winposition[1]] &&
+                        gamestate[winposition[1]] == gamestate[winposition[2]] &&
+                        gamestate[winposition[0]] != 2) {
+                    String winnerstr;
+                    gameactive = false;
+                    if (gamestate[winposition[0]] == 0) {
+                        winnerstr = "X is the winner";
+                    } else {
+                        winnerstr = "O is the winner";
+                    }
+                    TextView status = findViewById(R.id.status);
+                    status.setText(winnerstr);
                 }
-                else{
-                    winnerstr= "O is the winner";
-                }
-                TextView status =  findViewById(R.id.status);
-                status.setText(winnerstr);
             }
         }
-
-
     }
-
-
 
     public void gamereset(View view) {
         gameactive = true;
@@ -73,24 +76,28 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < gamestate.length; i++) {
             gamestate[i] = 2;
         }
-        ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView4)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView6)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView9)).setImageResource(0);
 
-    }
+        ImageView imageView1 = findViewById(R.id.imageView1);
+        ImageView imageView2 = findViewById(R.id.imageView2);
+        ImageView imageView3 = findViewById(R.id.imageView3);
+        ImageView imageView4 = findViewById(R.id.imageView4);
+        ImageView imageView5 = findViewById(R.id.imageView5);
+        ImageView imageView6 = findViewById(R.id.imageView6);
+        ImageView imageView7 = findViewById(R.id.imageView7);
+        ImageView imageView8 = findViewById(R.id.imageView8);
+        ImageView imageView9 = findViewById(R.id.imageView9);
 
+        imageView1.setImageDrawable(null);
+        imageView2.setImageDrawable(null);
+        imageView3.setImageDrawable(null);
+        imageView4.setImageDrawable(null);
+        imageView5.setImageDrawable(null);
+        imageView6.setImageDrawable(null);
+        imageView7.setImageDrawable(null);
+        imageView8.setImageDrawable(null);
+        imageView9.setImageDrawable(null);
 
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView status = findViewById(R.id.status);
+        status.setText("X's turn");
     }
 }
